035cdd0a8c179a39dc13981e8b1e3c488ccde839
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2022-02-26 01:01:31 -0500

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,12 +1,15 @@
 import json
 
 def lambda_handler(event, context):
+    longinformation = '''
+    <h1 style="color: #5e9ca0;">This website is deployed using a Lambda Function attached to a API Gateway</h1>
+    <h2 style="color: #2e6c80;">How is this possible you may ask?</h2>
+    <p>API Gateway provides tools for creating and documenting web APIs that route HTTP requests to Lambda functions. You can secure access to your API with authentication and authorization controls. Your APIs can serve traffic over the internet or can be accessible only within your VPC.
+    '''
+
     return {
-    "statusCode": 200,
-    "statusDescription": "200 OK",
-    "isBase64Encoded": False,
-    "headers": {
-        "Content-Type": "text/html"
-    },
-    "body": "<h1>Hello from Lambda!</h1> <p>AWS Lambda is a serverless, event-driven compute service that lets you run code for virtually any type of application or backend service without provisioning or managing servers. You can trigger Lambda from over 200 AWS services and software as a service (SaaS) applications, and only pay for what you use.</p>"
+        "statusCode": 200,
+        "headers": {'Content-Type': 'text/html'},   #it really works by Hector Duran!
+        "body": longinformation
     }
+
